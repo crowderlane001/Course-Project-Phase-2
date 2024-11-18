b0c069f2287bcba42e3c354f5c8ceef891c03534
crowderlane001 <122763205+crowderlane001@users.noreply.github.com>
2024-11-17 22:38:55 -0500

    Fix packages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["@types/js-cookie"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
