56bf45cac42392a218507e876ee88e9b60adac1c
Lawrence Folsom Smith <smit4407@eceprog5.ecn.purdue.edu>
2024-12-05 20:54:35 -0500

    auth updates

--- backend/src/functions/authenticate/index.ts
+++ backend/src/functions/authenticate/index.ts
@@ -117,6 +117,9 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
     return {
       statusCode: 200,
       headers: {
+        "Access-Control-Allow-Origin": "http://localhost:5173", // Allow requests from your frontend
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS", // Allow HTTP methods
+        "Access-Control-Allow-Headers": "Content-Type, Authorization", // Allow headers
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(`bearer ${token}`)
--- backend/src/functions/regex/index.ts
+++ backend/src/functions/regex/index.ts
@@ -216,6 +216,12 @@ export const handler = async (event: APIGatewayEvent, context: Context) => {
         // Respond with the filtered items
         return {
             statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "http://localhost:5173", // Allow requests from your frontend
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS", // Allow HTTP methods
+                "Access-Control-Allow-Headers": "Content-Type, Authorization", // Allow headers
+                'Content-Type': 'application/json'
+              },
             body: JSON.stringify(filteredItems) // Return filteredItems directly
         };
     } catch (error) {
