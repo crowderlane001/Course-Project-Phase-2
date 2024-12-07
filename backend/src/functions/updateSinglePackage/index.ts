7836e04709c84372b95d914dbdc8d1e6539c7bf4
Lawrence Folsom Smith <smit4407@eceprog1.ecn.purdue.edu>
2024-12-06 21:01:39 -0500

    create updates

--- backend/src/functions/updateSinglePackage/index.ts
+++ backend/src/functions/updateSinglePackage/index.ts
@@ -427,7 +427,7 @@ export async function handler(
     return {
       statusCode: 200,
       headers: {
-        "Access-Control-Allow-Origin": "http://localhost:5173", // Allow requests from your frontend
+        "Access-Control-Allow-Origin": "*", // Allow requests from your frontend
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS", // Allow HTTP methods
         "Access-Control-Allow-Headers": "Content-Type, X-Authorization", // Allow headers
         'Content-Type': 'application/json'
